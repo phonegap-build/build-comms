936f1cc279d53b0ee0009b21832c3505fdc19787
goya <brettrudd@gmail.com>
2019-01-15 14:10:12 -0800

    [msg] add log fields

--- lib/build_comms/message.rb
+++ lib/build_comms/message.rb
@@ -15,7 +15,8 @@ module BuildComms
       :time_built, :auth_token, :hydration_domain, :build_unique_id,
       :duration, :plugins, :debug_domain, :debug_key, :build_token,
       :s3_manifest_url, :s3_package_url, :s3_usage, :s3_key, :cache,
-      :compile_duration, :gimlet_server, :gimlet_version, :build_number
+      :compile_duration, :gimlet_server, :gimlet_version, :build_number,
+      :build_log, :gimlet_log
 
     @@default_pkg = "com.phonegap.www"
 
