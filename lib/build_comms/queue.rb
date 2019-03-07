74ea92ace2d8ce4dd167c7405ea25c7c95f2ba55
goya <brettrudd@gmail.com>
2019-03-06 16:53:46 -0800

    [sqs] add queue length

--- lib/build_comms/queue.rb
+++ lib/build_comms/queue.rb
@@ -6,6 +6,14 @@ module BuildComms
         @client || @client = Aws::SQS::Client.new
       end
 
+      def queue_length queue_name
+        opts = {
+          queue_url: client.get_queue_url({ queue_name: queue_name }).queue_url,
+          attribute_names: ["ApproximateNumberOfMessages"]
+        }
+        return client.get_queue_attributes(opts).attributes["ApproximateNumberOfMessages"]
+      end
+
       def queue queue_name, attrs = { :VisibilityTimeout => "300" }
         opts = { :queue_name => queue_name, :attributes => attrs }
         return Queue.new client.create_queue(opts).queue_url
